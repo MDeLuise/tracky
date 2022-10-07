496a4134aba3caec3abbe28a1cc903099220d8a8
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2022-10-07 14:56:44 +0200

    Organize deployment file

---
+++ deployment/run-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "waiting db to accept connections..."
+deployment/wait-for-it.sh -t 0 db:5432
+
+echo "creating database and doing migrations..."
+(buffalo pop create -a; buffalo pop migrate) 2> /dev/null
+
+echo "starting application..."
+/bin/app
