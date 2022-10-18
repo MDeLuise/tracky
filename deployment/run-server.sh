08072959e059969c6d2aef8ab85d45083a0f54c6
MDeLuise <66636702+MDeLuise@users.noreply.github.com>
2022-10-18 10:33:30 +0200

    Add sleep before start initial database migrations

--- deployment/run-server.sh
+++ deployment/run-server.sh
@@ -2,6 +2,7 @@
 
 echo "waiting db to accept connections..."
 deployment/wait-for-it.sh -t 0 db:5432
+sleep 5
 
 echo "creating database and doing migrations..."
 (buffalo pop create -a; buffalo pop migrate) 2> /dev/null
